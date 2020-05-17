bea1c24a6114e5762f186315947b851d3675cc22
mostcute <437043924@qq.com>
2020-05-18 00:27:56 +0800

    Update PFlog.go

--- PFlog.go
+++ PFlog.go
@@ -28,7 +28,7 @@ func (pf PrintFilelog)Printf(format string, v ...interface{})  {
 
 func init(){
 
-	file, err := os.OpenFile("./managerlog.txt",
+	file, err := os.OpenFile("/tmp/managerlog.txt",
 		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("Failed to open error log file:", err)
